35e40e792ab753d5bff2c23a007728637ab31da5
peekaboi <chicken.panda14@gmail.com>
2016-04-15 19:40:08 -0400

    Added extras selector with two choices
    Added random.org and meme generator to extras menu

--- HackForums/Live_editor.user.js
+++ HackForums/Live_editor.user.js
@@ -20,44 +20,43 @@ $("body").append("<link rel='stylesheet' type='text/css' href='http://cdn.wysibb
 wbbdebug = false;
 
 var settings = {
-    
     useHotkeys: GM_getValue("useHotkeys", false),
     useAutocorrect: GM_getValue("useAutocorrect", false),
-    customButtons: GM_getValue("customButtons", "bold,italic,underline,strike,hr,|,img,video,skype,ig,link,|,bullist,numlist,|,fontcolor,fontsize,fontfamily,|,justifyleft,justifycenter,justifyright,|,emotes,quote,code,pmme,spoiler,removeformat"),
-    
+    customButtons: GM_getValue("customButtons", "bold,italic,underline,strike,hr,|,img,video,skype,ig,link,|,bullist,numlist,|,fontcolor,fontsize,fontfamily,|,justifyleft,justifycenter,justifyright,|,emotes,quote,code,pmme,spoiler,removeformat | ,extras"),
+
     HTML: '<tr><td class="thead" colspan="2"><strong>Live Editor settings</strong></td><tr><td class="tcat">Use Hotkeys</td><td class="tcat"><input type="checkbox" name="useHotkeys" /></td></tr><tr><td class="tcat">Use autocorrect</td><td class="tcat"><input type="checkbox" name="useAutocorrect" /></td></tr></tr>',
-    
-    
+
+
     initForm: function() {
         $("#quick_reply_form table thead tr").after(settings.HTML);
         $("a[href='#settings']").html("Save settings");
-        
+
         if (settings.useHotkeys)
             $("input[type='checkbox'][name='useHotkeys']").attr("checked", "checked");
-        
+
         if (settings.useAutocorrect)
             $("input[type='checkbox'][name='useAutocorrect']").attr("checked", "checked");
-        
+
     },
-    
+
     saveForm: function() {
         $("#quick_reply_form table thead tr input:checked").each(function() {
             GM_setValue($(this).attr("name"), true);
         });
-        
+
         $("#quick_reply_form table thead tr input:not(:checked)").each(function() {
             GM_setValue($(this).attr("name"), false);
-        });         
-        
+        });
+
         $("#quick_reply_form table thead tr:gt(0)").remove();
         $("a[href='#settings']").html("Live Editor Settings");
     }
 }
 
 var wbbOpt = {
-    hotkeys: settings.useHotkeys, 
+    hotkeys: settings.useHotkeys,
     showHotkeys: settings.useHotkeys,
-    
+
     buttons: settings.customButtons,
     allButtons: {
         video: {
@@ -124,7 +123,7 @@ var wbbOpt = {
             }
         },
         code: {
-            buttonHTML: '<i class="fa fa-code fa-2x"></i>', 
+            buttonHTML: '<i class="fa fa-code fa-2x"></i>',
             transform : {
                 '<pre>{SELTEXT}</pre>':"[code]{SELTEXT}[/code]"
             }
@@ -136,12 +135,12 @@ var wbbOpt = {
                 title: "PM Me link",
                 width: "600px",
                 tabs: [
-                  {
-                    input: [ 
-                      {param: "SUBJECT",title:"Enter subject of the PM"},
-                      {param: "LINKTEXT",title:"Enter the link's text"}
-                    ]
-                  }
+                    {
+                        input: [
+                            {param: "SUBJECT",title:"Enter subject of the PM"},
+                            {param: "LINKTEXT",title:"Enter the link's text"}
+                        ]
+                    }
                 ],
             },
             transform: {
@@ -156,7 +155,7 @@ var wbbOpt = {
             }
         },
         quote: {
-            transform : { 
+            transform : {
                 '<blockquote><cite>AUTHOR Wrote: POST_ID</cite>{SELTEXT}</blockquote>':"[quote='AUTHOR' pid='PID']{SELTEXT}[/quote]"
             }
         },
@@ -167,11 +166,11 @@ var wbbOpt = {
                 title: "Skype button",
                 width: "600px",
                 tabs: [
-                  {
-                    input: [ 
-                      {param: "USERNAME",title:"Enter skype username"}
-                    ]
-                  }
+                    {
+                        input: [
+                            {param: "USERNAME",title:"Enter skype username"}
+                        ]
+                    }
                 ],
             },
             transform: {
@@ -185,11 +184,11 @@ var wbbOpt = {
                 title: "Instagram link",
                 width: "600px",
                 tabs: [
-                  {
-                    input: [ 
-                      {param: "LINK",title:"Instagram Link to photo/video"}
-                    ]
-                  }
+                    {
+                        input: [
+                            {param: "LINK",title:"Instagram Link to photo/video"}
+                        ]
+                    }
                 ],
             },
             transform: {
@@ -203,11 +202,27 @@ var wbbOpt = {
                 '<hr>':"[hr]"
             }
         },
-		emotes: {
-			type: 'select',
+        extras: {
+            type: 'select',
+            title: 'Extras',
+            options: 'extra_memegen,extra_random'
+        },
+        extra_memegen: {
+            title: "Meme Generator",
+            cmd: function() {
+                window.open('https://imgur.com/memegen', '_blank');
+            }
+        },extra_random: {
+            title: "Random.org",
+            cmd: function() {
+                window.open('https://random.org', '_blank');
+            }
+        },
+        emotes: {
+            type: 'select',
             title: "Add emotes",
-			options: "emote_pinch,emote_victoire,emote_hehe,emote_oui,emote_pleure,emote_ohmy,emote_blink,emote_superman,emote_nono,emote_biggrin,emote_sad,emote_unsure,emote_glare,emote_roflmao,emote_devlish,emote_rolleyes,emote_cool,emote_gratte,emote_confused,emote_bh,emote_ninja,emote_blush,emote_lipssealed,emote_yeye,emote_non,emote_smile,emote_whistle,emote_sleep,emote_evilgrin,emote_omg,emote_tongue,emote_mad,emote_huh,emote_thumbsup,emote_wacko,emote_pirate"
-		},
+            options: "emote_pinch,emote_victoire,emote_hehe,emote_oui,emote_pleure,emote_ohmy,emote_blink,emote_superman,emote_nono,emote_biggrin,emote_sad,emote_unsure,emote_glare,emote_roflmao,emote_devlish,emote_rolleyes,emote_cool,emote_gratte,emote_confused,emote_bh,emote_ninja,emote_blush,emote_lipssealed,emote_yeye,emote_non,emote_smile,emote_whistle,emote_sleep,emote_evilgrin,emote_omg,emote_tongue,emote_mad,emote_huh,emote_thumbsup,emote_wacko,emote_pirate"
+        },
         emote_pinch: {
             transform: {
                 '<img src="images/smilies/pinch.gif">':':pinch:'
@@ -217,87 +232,87 @@ var wbbOpt = {
             transform: {
                 '<img src="images/smilies/victoire.gif">': ':victoire:'
             }
-        }, 
+        },
         emote_hehe: {
             transform: {
                 '<img src="images/smilies/hehe.gif">': ':hehe:'
             }
-        }, 
+        },
         emote_oui: {
             transform: {
                 '<img src="images/smilies/oui.gif">': ':oui:'
             }
-        }, 
+        },
         emote_pleure: {
             transform: {
                 '<img src="images/smilies/bebe-pleure.gif">': ':bebe-pleure:'
             }
-        }, 
+        },
         emote_ohmy: {
             transform: {
                 '<img src="images/smilies/ohmy.gif">': ':ohmy:'
             }
-        }, 
+        },
         emote_blink: {
             transform: {
                 '<img src="images/smilies/blink.gif">': ':blink:'
             }
-        }, 
+        },
         emote_superman: {
             transform: {
                 '<img src="images/smilies/superman.gif">': ':superman:'
             }
-        }, 
+        },
         emote_nono: {
             transform: {
                 '<img src="images/smilies/nono.gif">': ':nono:'
             }
-        }, 
+        },
         emote_biggrin: {
             transform: {
                 '<img src="images/smilies/biggrin.gif">': ':biggrin:'
             }
-        }, 
+        },
         emote_sad: {
             transform: {
                 '<img src="images/smilies/sad.gif">': ':sad:'
             }
-        }, 
+        },
         emote_unsure: {
             transform: {
                 '<img src="images/smilies/unsure.gif">': ':unsure:'
             }
-        }, 
+        },
         emote_glare: {
             transform: {
                 '<img src="images/smilies/glare.gif">': ':glare:'
             }
-        }, 
+        },
         emote_roflmao: {
             transform: {
                 '<img src="images/smilies/roflmao.gif">': ':roflmao:'
             }
-        }, 
+        },
         emote_devlish: {
             transform: {
                 '<img src="images/smilies/devlish.gif">': ':devlish:'
             }
-        }, 
+        },
         emote_rolleyes: {
             transform: {
                 '<img src="images/smilies/rolleyes.gif">': ':rolleyes:'
             }
-        }, 
+        },
         emote_cool: {
             transform: {
                 '<img src="images/smilies/cool.gif">': ':cool:'
             }
-        }, 
+        },
         emote_gratte: {
             transform: {
                 '<img src="images/smilies/gratte.gif">': ':gratte:'
             }
-        }, 
+        },
         emote_confused: {
             transform: {
                 '<img src="images/smilies/confused.gif">': ':confused:'
@@ -307,82 +322,82 @@ var wbbOpt = {
             transform: {
                 '<img src="images/smilies/blackhat.gif">': ':blackhat:'
             }
-        }, 
+        },
         emote_ninja: {
             transform: {
                 '<img src="images/smilies/ninja.gif">': ':ninja:'
             }
-        }, 
+        },
         emote_blush: {
             transform: {
                 '<img src="images/smilies/blush.gif">': ':blush:'
             }
-        }, 
+        },
         emote_lipssealed: {
             transform: {
                 '<img src="images/smilies/lipssealed.gif">': ':lipssealed:'
             }
-        }, 
+        },
         emote_yeye: {
             transform: {
                 '<img src="images/smilies/yeye.gif">': ':yeye:'
             }
-        }, 
+        },
         emote_non: {
             transform: {
                 '<img src="images/smilies/non.gif">': ':non:'
             }
-        }, 
+        },
         emote_smile: {
             transform: {
                 '<img src="images/smilies/smile.gif">': ':smile:'
             }
-        }, 
+        },
         emote_whistle: {
             transform: {
                 '<img src="images/smilies/whistle.gif">': ':whistle:'
             }
-        }, 
+        },
         emote_sleep: {
             transform: {
                 '<img src="images/smilies/sleep.gif">': ':sleep:'
             }
-        }, 
+        },
         emote_evilgrin: {
             transform: {
                 '<img src="images/smilies/evilgrin.gif">': ':evilgrin:'
             }
-        }, 
+        },
         emote_omg: {
             transform: {
                 '<img src="images/smilies/omg.gif">': ':omg:'
             }
-        }, 
+        },
         emote_tongue: {
             transform: {
                 '<img src="images/smilies/tongue.gif">': ':tongue:'
             }
-        }, 
+        },
         emote_mad: {
             transform: {
                 '<img src="images/smilies/mad.gif">': ':mad:'
             }
-        }, 
+        },
         emote_huh: {
             transform: {
                 '<img src="images/smilies/huh.gif">': ':huh:'
             }
-        }, 
+        },
         emote_thumbsup: {
             transform: {
                 '<img src="images/smilies/thumbsup.gif">': ':thumbsup:'
             }
-        }, 
+        },
         emote_wacko: {
             transform: {
                 '<img src="images/smilies/wacko.gif">': ':wacko:'
             }
-        }, 
+        },
         emote_pirate: {
             transform: {
                 '<img src="images/smilies/pirate.gif">': ':pirate:'
@@ -391,36 +406,38 @@ var wbbOpt = {
     }
 }
 
+
+
 $(document).ready(function() {
     $("#message").wysibb(wbbOpt);
-    
+
     $("#quick_reply_form table thead td").append("<a class='smalltext float_right' href='#settings'>Live Editor Settings</a>");
-    
+
     if (settings.useAutocorrect === false) {
         $(".wysibb-text-editor").attr("autocomplete", "off");
         $(".wysibb-text-editor").attr("autocorrect", "off");
         $(".wysibb-text-editor").attr("autocapitalize", "off");
         $(".wysibb-text-editor").attr("spellcheck", "false");
     }
-    
+
     $("a[href='#settings']").on("click", function(e) {
         e.preventDefault();
-        
+
         if ($("input[type='checkbox'][name='useHotkeys']").length === 0) {
             settings.initForm();
         } else {
             settings.saveForm();
         }
-        
+
         return false;
     });
-    
+
     $("body").on("keydown", function(e) {
         if (e.key === "Tab" || e.keyCode === "9" || e.which === "9") {
             $("#quick_reply_submit").focus();
         }
     });
-    
+
     $("#quick_reply_submit").on("click", function() {
         $("#posts").bind("DOMNodeInserted", function(e) {
             if (e.target.tagName === "DIV") {
